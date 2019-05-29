3909035020ddde2af84199fab05da442b9b39f51
Kush <abhinavkushagra@gmail.com>
2019-05-29 18:27:14 +0530

    Initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropType from "prop-types";
+import Todo from "./Todo";
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map((todo, index) => (
+      <Todo key={index} {...todo} onClick={() => (onTodoClick = index)} />
+    ))}
+  </ul>
+);
+
+TodoList.propType = {
+  todos: PropType.arrayOf(
+    PropType.shape({
+      id: PropType.number.isRequired,
+      completed: PropType.bool.isRequired,
+      text: PropType.string.isRequired
+    }).isRequired
+  ).isRequired,
+  onTodoClick: PropType.func.isRequired
+};
